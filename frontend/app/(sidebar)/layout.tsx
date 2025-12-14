b67f5791ce393f65fb9f7bd2698de738f235780f
Cohii <hengjunxiangg@gmail.com>
2025-12-14 16:53:28 +0800

    Add Sidebar

---
+++ frontend/app/(sidebar)/layout.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import Sidebar from "@/components/layout/Sidebar";
+
+export default function Layout({
+    children
+}: {
+    children: React.ReactNode
+}) {
+    return(
+        <>
+            <Sidebar />
+            <div>{children}</div>
+        </>
+    );
+}
